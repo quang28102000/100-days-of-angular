4d77d9b1d07dfd3d24a46907148bec2136d76c44
Minh Quang <minhquang@Minhs-MacBook-Pro.local>
2023-01-02 18:56:46 +0700

    Explore Angular's component, bootstrap and @ngModule

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular100day app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('The app is running');
   });
 });
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  'selector': 'app-welcome',
+  template: '<span>Welcome app works!</span>'
+})
+export class WelcomeComponent {
+
+}
